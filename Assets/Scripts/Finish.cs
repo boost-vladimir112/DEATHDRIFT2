d7a3da43bf379f7a346f9292d5b4e6002301c63d
Andrey <78420479+boost-vladimir112@users.noreply.github.com>
2025-03-16 15:50:37 +0300

    игра на паузу

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using PG;
 
 public class Finish : MonoBehaviour
 {
     private List<GameObject> finishedCars = new List<GameObject>(); // Очередность машин на финише
     [SerializeField] private TextMeshProUGUI winText;
+    [SerializeField] private GameObject winPanel;
+   [SerializeField] GameController gameController;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +22,12 @@ public class Finish : MonoBehaviour
                 {
                     int position = finishedCars.Count; // Определяем место игрока
                     winText.text = position.ToString();
+                    winPanel.SetActive(true);
+
+                    AudioListener.volume = 0;
+                    Time.timeScale = 0f;
+
+
 
                     Debug.Log("Игрок финишировал на месте: " + position);
                 }
--- Assets/UniversalVehicleController/Scripts/GamePlay/GameController.cs
+++ Assets/UniversalVehicleController/Scripts/GamePlay/GameController.cs
@@ -97,10 +97,14 @@ namespace PG
         public void RestartScene()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            AudioListener.volume = 1;
+            Time.timeScale = 1f;
         }
         public void LoadMenuScene()
         {
             SceneManager.LoadScene(0);
+            AudioListener.volume = 1;
+            Time.timeScale = 1f;
         }
 
         public void ChangeTimeScale(float delta)
