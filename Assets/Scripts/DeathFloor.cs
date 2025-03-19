1de68aafa811614bf55e84c76be776348a374c77
Andrey <78420479+boost-vladimir112@users.noreply.github.com>
2025-03-19 16:48:34 +0300

    3level end

---
+++ Assets/Scripts/DeathFloor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DeathFloor : MonoBehaviour
+{
+    [SerializeField] private GameObject LosePanel;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerCar"))
+        {
+            LosePanel.SetActive(true);
+
+            AudioListener.pause = true;
+            Time.timeScale = 0f;
+        }
+    }
+}
